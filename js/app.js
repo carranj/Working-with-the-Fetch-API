7d58129f903583e837cad00459dc65910532d97e
Jose Carranco <josecarranco@joses-mbp.attlocal.net>
2020-05-16 00:12:07 -0500

    Manage Multiple Requests with Promise.all

--- js/app.js
+++ js/app.js
@@ -12,11 +12,20 @@ function fetchData(url){
             .catch(error => console.log('Looks like there was a problem', error))
 }
 
-fetchData('https://dog.ceo/api/breeds/list')
-    .then(data => generateOptions(data.message))    
+Promise.all([
+    fetchData('https://dog.ceo/api/breeds/list'),
+    fetchData('https://dog.ceo/api/breeds/image/random')
 
-fetchData('https://dog.ceo/api/breeds/image/random')
-    .then(data => generateImage(data.message))
+])
+.then(data => {
+        const breedList = data[0].message;
+        const randomImage = data[1].message;
+
+        generateOptions(breedList);
+        generateImage(randomImage);
+    }
+)    
+
 
 // ------------------------------------------
 //  HELPER FUNCTIONS
